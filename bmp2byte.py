73fd9f882400fce75ef4c756a637f03a8262d3bf
Matthew Tunstall <git@deltanova.co.uk>
2015-11-01 14:11:42 +0000

    Big refactoring of code - Working

--- bmp2byte.py
+++ bmp2byte.py
@@ -18,7 +18,10 @@ def main():
     ####################################################################
     data = get_data(img_file)
     width,height = extract_img_data(data)
-    page_count = calculate_pages(width,height,page_height_px)
+    # TODO: Check image size. The script is designed around images where
+    #       the height and width are multiples of 8. If the image does
+    #       not meet this criteria then an extra stage of padding the
+    #       image with additional data will be required.
     data2 = convert_mode(data)
     data3 = create_data_array(data2)
     checked_data = check_data_len(data3,8192)
@@ -29,25 +32,24 @@ def main():
     ####################################################################
     # Stage 2: Split Pixel Data into Display Pages
     ####################################################################
-
+    # Calculate the number of display pages based on image size
+    page_count = calculate_pages(width,height,page_height_px)
+    pixels_per_page = page_height_px * width
     # With a large image there is alot of data to process. Let's split it up into pages before we go any furthur.
-    pages = []
-    bits_per_page = page_height * width
-    i = 0
-    for p in data3:
-        page = []
-
-        while (i < bits_per_page):
-            page.append(p)
-            i = i + 1
-        i = 0
-        pages.append(page)
-    #print("Pages: ")
-    print(pages)
-    ####################################################################
-    # Stage 3: Reformat Display Page into Verticle Columns
-    ####################################################################
+    pages = get_pages(page_count,pixels_per_page,data4)
+    #TODO: change width variable as this is taken from the original
+    #      image. If this value is not a multiple of 8 then the code
+    #      might not work. Once image padding is implemented for images
+    #      that require padding, the width variable should refer to the
+    #      padded value.
+    converted_pages = convert_pages(pages,width,page_height_px)
+    image_data = converted_image(converted_pages)
+    #print(image_data)
     """
+    ####################################################################
+    # Stage 3: Reformat Display Page into Vertical Columns
+    ####################################################################
+
     page_cols = []
     #for each_page in pages:
 
@@ -75,48 +77,42 @@ def main():
     print(cols)
     #print(page_cols)
     """
-########################################################################
-"""
-    # The columns array contains 8 pixel arrays for each column. The values in each sub array need to be converted first into binary then into hex.
-    page_hex = []
-    for page in page_cols:
-        hex_values = []
-        for c in columns:
-            ##print(c)
-            d = 0
-            #for c in cmn:
-            if c[0] == 1:
-                d = d + 128
-            if c[1] == 1:
-                d = d + 64
-            if c[2] == 1:
-                d = d +32
-            if c[3] == 1:
-                d = d + 16
-            if c[4] == 1:
-                d = d + 8
-            if c[5] == 1:
-                d = d + 4
-            if c[6] == 1:
-                d = d + 2
-            if c[7] == 1:
-                d = d + 1
-            h = hex(d)
-            hex_values.append(h)
-        page_hex.append(hex_values)
+
+    """
+    ####################################################################
+
+
     #rint(len(page_hex))
         #print(hex_values)
 
-
+    """
     f = open('output.txt','wb')
-    for c in range(0,page_count):
-        for h in page_hex[c]:
-            f.write(str(h) + ' ,')
-        f.write('\n')
-    f.close()
-"""
+    #for c in range(0,page_count):
+    row_count = 0
+    row_count2 = 0
+    for h in image_data:
 
-"""
+        if (len(h) == 4):
+            pass
+            #f.write(str(h) + ' ,')
+        else: #Add missing leading zero
+            h = h[:2] + '0' + h[2:]
+        f.write(str(h) + ', ')
+
+        row_count = row_count + 1
+
+        # Add additional separators to output
+        if row_count == 16:
+            f.write('\n')
+            row_count = 0
+            row_count2 = row_count2 + 1
+        if row_count2 == 8:
+            f.write('\n')
+            row_count2 = 0
+
+    f.close()
+
+    """
     for page[0] in page_hex:
 
     # Next step - reformat hex_value array into comma seperated sting of 2 digit hex byte values eg: 0F, 12, E3, ...
@@ -175,7 +171,7 @@ def main():
                 print(str(h) + ', ')
         #print("\n")
     #f.close()
-"""
+    """
 def get_data(data_file):
     f = Image.open(data_file)
     return f
@@ -224,8 +220,79 @@ def modify_data_values(data):
         else:
             data_array.append(1)
     return data_array
+def get_pages(page_count,pixels_per_page,data):
+    pages = []
+    array_pointer = 0
+    i = 0
+    while (len(pages) < 8):
+        page = []
 
+        while (i < pixels_per_page):
+            page.append(data[array_pointer])
+            array_pointer = array_pointer + 1
+            i = i + 1
+        i = 0
+        pages.append(page)
 
+    #print(pages)
+    return pages
+def convert_pages(pages, page_width, page_height):
+    # Convert the data for each page into column data
+    converted_pages = []
+    for page in pages:
+        # Change the data format from rows into columns
+        p = page2col(page, page_width, page_height)
+        # p now contains lists of the data within each column [MSB ... LSB]
+        # The column data is in binary. We want to convert it to hex.
+        hex_page = col2hex(p)
+        converted_pages.append(hex_page)
+    return converted_pages
+def page2col(page_data, page_width, page_height):
+    cols = []
+    column_count = 0
+    # Reuse the get_pages() function to split the page down into rows
+    rows = get_pages(page_height, page_width, page_data)
+    while (column_count < page_width):
+        column_binary = []
+        for row in rows:
+            column_binary.append(row[column_count])
+        column_binary.reverse()
+        cols.append(column_binary)
+        column_count = column_count + 1
+    # cols now contains lists of the data within each column [MSB ... LSB]
+    return cols
+def col2hex(col_data):
+    # col_data contains 8 pixel lists for each column MSB..LSB. The values
+    # in each list need to be converted first into decimal then into hex.
+    col_hex = []
+    for c in col_data:
+        d = 0           # Decimal Value
+        # Apply binary weighting to each bit
+        if c[0] == 1:
+            d = d + 128
+        if c[1] == 1:
+            d = d + 64
+        if c[2] == 1:
+            d = d +32
+        if c[3] == 1:
+            d = d + 16
+        if c[4] == 1:
+            d = d + 8
+        if c[5] == 1:
+            d = d + 4
+        if c[6] == 1:
+            d = d + 2
+        if c[7] == 1:
+            d = d + 1
+        h = hex(d)      # Decimal to Hex Conversion
+        col_hex.append(h)
+    return col_hex
+def converted_image(converted_pages):
+    image_hex= []
+    for page in converted_pages:
+        for col in page:
+            image_hex.append(col)
+    return image_hex
 
 if __name__ == "__main__":
     main()
